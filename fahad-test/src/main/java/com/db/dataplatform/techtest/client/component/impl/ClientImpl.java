ee3b19e61fd37f56b2f462b809b7496cb5ab6a34
Fahad Sheikh <fahadbuddy@gmail.com>
2021-01-24 22:44:19 +0000

    added MEDIATYPE JSON content and accept headers

--- fahad-test/src/main/java/com/db/dataplatform/techtest/client/component/impl/ClientImpl.java
+++ fahad-test/src/main/java/com/db/dataplatform/techtest/client/component/impl/ClientImpl.java
@@ -1,12 +1,13 @@
 package com.db.dataplatform.techtest.client.component.impl;
 
-import com.db.dataplatform.techtest.Constant;
 import com.db.dataplatform.techtest.client.api.model.DataEnvelope;
 import com.db.dataplatform.techtest.client.component.Client;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -15,6 +16,7 @@ import javax.validation.Valid;
 import java.util.List;
 
 import static com.db.dataplatform.techtest.Constant.URI_PUSHDATA;
+import static java.util.Collections.singletonList;
 
 /**
  * Client code does not require any test coverage
@@ -33,7 +35,11 @@ public class ClientImpl implements Client {
     public void pushData(@Valid DataEnvelope dataEnvelope) {
         log.info("Pushing data {} to {}", dataEnvelope.getDataHeader().getName(), URI_PUSHDATA);
 
-        HttpEntity<DataEnvelope> body = new HttpEntity<>(dataEnvelope);
+        HttpHeaders requestHeaders = new HttpHeaders();
+        requestHeaders.setAccept(singletonList(MediaType.APPLICATION_JSON));
+        requestHeaders.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<DataEnvelope> body = new HttpEntity<>(dataEnvelope, requestHeaders);
 
         restTemplate.postForLocation(URI_PUSHDATA, body);
 
